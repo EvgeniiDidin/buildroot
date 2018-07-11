033366c012bd4a4fcca2330f7d7cf41a922ac50f
Evgeniy Didin <Evgeniy.Didin@synopsys.com>
2018-07-11 13:46:32 +0300

    switch ltp-testsuite to arc version if arch=arc
    Signed-off-by: Evgeniy Didin <Evgeniy.Didin@synopsys.com>

--- package/ltp-testsuite/ltp-testsuite.mk
+++ package/ltp-testsuite/ltp-testsuite.mk
@@ -4,9 +4,16 @@
 #
 ################################################################################
 
+ifeq ($(BR2_arc),y)
+LTP_TESTSUITE_VERSION = master
+LTP_TESTSUITE_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,ltp,$(LTP_TESTSUITE_VERSION))
+LTP_TESTSUITE_SOURCE = ltp-$(LTP_TESTSUITE_VERSION).tar.gz
+else
+
 LTP_TESTSUITE_VERSION = 20180118
 LTP_TESTSUITE_SOURCE = ltp-full-$(LTP_TESTSUITE_VERSION).tar.xz
 LTP_TESTSUITE_SITE = https://github.com/linux-test-project/ltp/releases/download/$(LTP_TESTSUITE_VERSION)
+endif
 LTP_TESTSUITE_LICENSE = GPL-2.0, GPL-2.0+
 LTP_TESTSUITE_LICENSE_FILES = COPYING
 
@@ -30,6 +37,16 @@ ifeq ($(BR2_PACKAGE_LIBCAP)$(BR2_PACKAGE_ATTR),yy)
 LTP_TESTSUITE_DEPENDENCIES += libcap
 else
 LTP_TESTSUITE_CONF_ENV += ac_cv_lib_cap_cap_compare=no
+endif
+
+ifeq ($(BR2_arc),y)
+LTP_TESTSUITE_CONF_OPTS += --enable-arc-support
+
+define LTP_TESTSUITE_PRECONFIGURE
+       make -C $(@D) autotools
+endef
+
+LTP_TESTSUITE_PRE_CONFIGURE_HOOKS += LTP_TESTSUITE_PRECONFIGURE
 endif
 
 # No explicit enable/disable options
