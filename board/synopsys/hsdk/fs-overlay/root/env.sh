4d719c0de949d9a08bed1b9e804731104e84beba
Evgeniy Didin <Evgeniy.Didin@synopsys.com>
2019-09-17 20:50:47 +0300

    Add overlays for demo
    Signed-off-by: Evgeniy Didin <Evgeniy.Didin@synopsys.com>

---
+++ board/synopsys/hsdk/fs-overlay/root/env.sh
@@ -0,0 +1,9 @@
+export GST_GL_API=opengl
+export GST_PLUGIN_SYSTEM_PATH=/usr/lib32/gstreamer-1.0
+export GST_GL_PLATFORM=wgl
+export  GST_GL_WINDOW=wayland
+export  XDG_RUNTIME_DIR=/root/weston
+export QT_QPA_PLATFORM=wayland-egl
+export MESA_LOADER_DRIVER_OVERRIDE=imx-drm
+export  XDG_RUNTIME_DIR=/root/weston
+
